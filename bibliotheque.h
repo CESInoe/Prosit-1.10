12bc840f6feea2782f7d40718cc784bba4a2ced5
CESInoe <33095171+CESInoe@users.noreply.github.com>
2017-10-25 19:49:19 +0200

    Update bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -3,6 +3,10 @@
 
 void read ();
 void divide (int numerator, int denominator, int numDeux, int denDeux);
+void multiply (int numerator, int denominator, int numDeux, int denDeux);
+void add (int numerator, int denominator, int numDeux, int denDeux);
+void substract (int numerator, int denominator, int numDeux, int denDeux);
+void simplify (int numerator, int denominator, int numDeux, int denDeux);
 
 
 #endif // BIBLIOTHEQUE_H_INCLUDED
