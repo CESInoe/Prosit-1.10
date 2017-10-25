bf62078189fee434d538aeb57c657ae3a5f0c9d1
CESInoe <33095171+CESInoe@users.noreply.github.com>
2017-10-25 19:46:13 +0200

    Update bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -2,7 +2,7 @@
 #define BIBLIOTHEQUE_H_INCLUDED
 
 void read ();
-void divide (int numerator, int denominator, int numDeux, int denDeux)*
+void divide (int numerator, int denominator, int numDeux, int denDeux);
 
 
 #endif // BIBLIOTHEQUE_H_INCLUDED
