e2569b262ee9bc89e497d48796494b66536a620b
CESInoe <33095171+CESInoe@users.noreply.github.com>
2017-10-25 19:46:02 +0200

    Update bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,6 +1,8 @@
 #ifndef BIBLIOTHEQUE_H_INCLUDED
 #define BIBLIOTHEQUE_H_INCLUDED
 
+void read ();
+void divide (int numerator, int denominator, int numDeux, int denDeux)*
 
 
 #endif // BIBLIOTHEQUE_H_INCLUDED
